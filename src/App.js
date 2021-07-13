b754658ad8fc9b692baa07fe8233da963be54cc2
s110719005 <48974204+s110719005@users.noreply.github.com>
2021-07-13 22:04:18 +0800

    ADD product page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { StoreProvider } from "./store";
 import Home from './pages/Home';
 import SignInUp from './pages/SignInUp';
 import Account from './pages/Account';
+import Product from './pages/Product';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route exact path="/Account" component={Account} />
           <Route exact path="/SignInUp" component={SignInUp}/>
+          <Route exact path="/Product" component={Product}/>
         </Switch>
       </BrowserRouter>
     </StoreProvider>
---
+++ src/pages/Product.js
@@ -0,0 +1,15 @@
+import { useContext,useState } from "react";
+import { Layout } from 'antd';
+import JJHeader from "../components/Header";
+
+
+function Product() {
+  return (
+    <div className="login-main-area">
+        <JJHeader/>
+        Product
+    </div>
+  );
+}
+
+export default Product;
