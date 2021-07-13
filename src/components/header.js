9d32f37fd188e47386dec5fdb076f6a04a56d8c2
xinyun <xinyun32889@gmail.com>
2021-07-13 22:37:29 +0800

    heder登入登出文字變化可可

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,65 @@
-import { useState,useEffect, useContext  } from "react";
+import { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 import { useHistory } from "react-router-dom";
 
-import { StoreContext } from "../store"
-
+import { StoreContext } from "../store";
 
 export default function Header() {
-  const { state: { userSignin : { userInfo, remember } } } = useContext(StoreContext);
+  const {
+    state: {
+      userSignin: { userInfo, remember },
+    },
+  } = useContext(StoreContext);
   const history = useHistory();
   useEffect(() => {
-    if(remember)
-       localStorage.setItem("userInfo", JSON.stringify(userInfo));
-    else
-     localStorage.removeItem("userInfo");
- }, [userInfo, remember]);
+    if (remember) localStorage.setItem("userInfo", JSON.stringify(userInfo));
+    else localStorage.removeItem("userInfo");
+  }, [userInfo, remember]);
   return (
     <div className="header text-white">
-      <div>
-          淨・靚
+      <div className="header text-white">
+        {userInfo === null ? (
+          <div>
+            <div>淨・靚</div>
+            <Link to="/">
+              <div className="text-white">首頁</div>
+            </Link>
+            <Link to="/">
+              <div className="text-white">商品</div>
+            </Link>
+            <Link to="/">
+              <div className="text-white">關於</div>
+            </Link>
+
+            <Link to="/SignInUp?redirect=Account">
+              <div className="text-white">登入/註冊</div>
+            </Link>
+            <Link to="/SignInUp?redirect=Account">
+              <div className="text-white">購物車</div>
+            </Link>
+          </div>
+        ) : (
+          <div>
+            <div>淨・靚</div>
+            <Link to="/">
+              <div className="text-white">首頁</div>
+            </Link>
+            <Link to="/">
+              <div className="text-white">商品</div>
+            </Link>
+            <Link to="/">
+              <div className="text-white">關於</div>
+            </Link>
+
+            <Link to="/SignInUp?redirect=Account">
+              <div className="text-white">會員</div>
+            </Link>
+            <Link to="/SignInUp?redirect=Account">
+              <div className="text-white">購物車</div>
+            </Link>
+          </div>
+        )}
       </div>
-      I am header
-      <Link to="/">
-        <div className="text-white">
-            首頁
-        </div>
-        </Link>
-      <Link to="/SignInUp?redirect=Account">
-        <div className="text-white">
-            登入/註冊
-        </div>
-        </Link>
     </div>
   );
 }
