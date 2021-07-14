d6d556142e461e20f4111c7eabe7127fef21e025
xinyun <xinyun32889@gmail.com>
2021-07-14 11:07:04 +0800

    slide完成

---
+++ src/components/slider.js
@@ -0,0 +1,42 @@
+import { Carousel } from "antd";
+export default function Slider() {
+  return (
+    <Carousel autoplay>
+      <div>
+        <img
+          src="https://pbs.twimg.com/media/D4kYK6MVUAATuUT.jpg"
+          alt="Cart"
+          className="test"
+        />
+      </div>
+      <div>
+        <img
+          src="https://i.pinimg.com/originals/da/1a/6d/da1a6d028b87ef9175946836bdc4bf37.jpg"
+          alt="Cart"
+          className="test"
+        />
+      </div>
+      <div>
+        <img
+          src="https://i.pinimg.com/originals/b4/9c/b7/b49cb730c6f76253561ac5d4acec33c5.jpg"
+          alt="Cart"
+          className="test"
+        />
+      </div>
+      <div>
+        <img
+          src="https://i.pinimg.com/originals/5a/54/c2/5a54c230575f39b65f9e2730504e339e.jpg"
+          alt="Cart"
+          className="test"
+        />
+      </div>
+      <div>
+        <img
+          src="https://i.pinimg.com/originals/b4/9c/b7/b49cb730c6f76253561ac5d4acec33c5.jpg"
+          alt="Cart"
+          className="test"
+        />
+      </div>
+    </Carousel>
+  );
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,9 @@
 import { useContext, useState } from "react";
 import { Layout } from "antd";
 import JJHeader from "../components/HeaderNew";
+import JJSlider from "../components/Slider";
 import JJFooter from "../components/Footer";
+
 //import { StoreContext } from "../store"
 
 const { Header, Content, Footer } = Layout;
@@ -10,7 +12,7 @@ function Home() {
   return (
     <Layout>
       <JJHeader />
-
+      <JJSlider />
       <JJFooter />
     </Layout>
   );
