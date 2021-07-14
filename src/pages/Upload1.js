a01ac921fffa39c82576c5cfc897eb773a3f727d
s110719005 <48974204+s110719005@users.noreply.github.com>
2021-07-14 23:41:15 +0800

    FINISH upload page 1

--- src/pages/Upload1.js
+++ src/pages/Upload1.js
@@ -20,7 +20,32 @@ function Upload1() {
         <JJHeader />
       <div className="mainarea upload">
           <div className="donate-title">上架</div>
-            
+          <div className="upload-step1">
+            <div className="upload-step1-selection">
+                <img
+                        className="upload-notgift-btn"
+                        src="https://github.com/Singularity-v/7th-Hach/blob/main/DONATE&SUS/not_gift.png?raw=true"
+                        alt="add button" 
+                    />
+                    <div className="upload-step1-hint">
+                        <div className="upload-line"></div>
+                        <div className="upload-text">&nbsp;&nbsp;&nbsp;是否為贈品&nbsp;&nbsp;&nbsp;</div>
+                        <div className="upload-line"></div>
+                    </div>
+                <img
+                        className="upload-gift-btn"
+                        src="https://github.com/Singularity-v/7th-Hach/blob/main/DONATE&SUS/gift.png?raw=true"
+                        alt="add button" 
+                    />
+            </div>
+            <div className="upload-step1-pic-container">
+                <img
+                    className="upload-step1-pic"
+                    src="https://github.com/Singularity-v/7th-Hach/blob/main/DONATE&SUS/sell_step_01.png?raw=true"
+                    alt="add button" 
+                />
+            </div>
+          </div>
 
           
       </div>
