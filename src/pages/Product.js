b888a435fc2eebdcd974552ee914454b013a78f9
s110719005 <48974204+s110719005@users.noreply.github.com>
2021-07-14 10:18:52 +0800

    ADD product item

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -7,8 +7,13 @@ import { StoreContext } from "../store"
 import {setPage} from "../actions"
 import {getTitle} from "../utils"
 import CategoryCard from "../components/Product/CategoryCard";
+import CategoryItem from "../components/Product/CategoryItem";
 
-
+import { Select } from 'antd';
+const { Option } = Select;
+  function handleChange(value) {
+    console.log(`selected ${value}`);
+  }
 
 function Product() {
   const { state: { page: { title, products } } , dispatch} = useContext(StoreContext);
@@ -17,14 +22,36 @@ function Product() {
     console.log(url);
     setPage(dispatch,url, getTitle(url));
   }, []);// eslint-disable-line react-hooks/exhaustive-deps 
+  
   return (
+    
     <div className="">
         <JJHeader/>
         <div className="mainarea">
-          Product
           <CategoryCard/>
-          <div className="category-current-name">
-            {title}
+          <div className="category-subtitle-container">
+            <div className="category-current-name">
+              {title}
+            </div>
+            <div>
+              <div>
+                <CategoryItem to="/Product/All" className="category-select-all" activeClassName="category-select-active">
+                  查看全部
+                </CategoryItem>
+              </div>
+              <Select defaultValue="ALL" 
+                style={{ 
+                    width: 150, 
+                    borderRadius:"50px",
+                }} 
+                onChange={handleChange}
+                >
+                    <Option value="ALL">ALL</Option>
+                    <Option value="BABY">BABY</Option>
+                    <Option value="BUBBLE">BUBBLE</Option>
+                    <Option value="WINTER">WINTER</Option>
+                </Select>
+            </div>
           </div>
           <ProductList/>
         </div>
